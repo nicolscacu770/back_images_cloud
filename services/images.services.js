d6a15ac3cfdc433c28226448c7d8cae019e21e28
nicolscacu770 <nicolas.cacua@uptc.edu.co>
2023-12-19 14:25:35 -0500

    fix deletear endpoint

--- services/images.services.js
+++ services/images.services.js
@@ -62,54 +62,15 @@ module.exports = {
 
     update : async(req,res) => {
         const {id} = req.params;
-        let dateAct = new Date();
-        let founded;
-
-        const fechaHoy = `${dateAct.getDate()}/${dateAct.getMonth()}/${dateAct.getFullYear()}`
-        const horaActual = `${dateAct.getHours()}:${dateAct.getMinutes()}:${dateAct.getSeconds()}`;
-           
-        const { name, type, value} = req.body
-        const data = {"id": id, "nombre": name, "tipo": type, "valor": value, "fecha": fechaHoy, "hora": horaActual}
-
-        try {
-            for (let pos = 0; pos < datos.imagens.length; pos++) {
-                if(datos.imagens[pos].id == id){
-                    datos.imagens[pos] = data; 
-                    founded = true;
-                }
-            }
-
-            if(founded){
-                
-                return res.status(200).json({"state": true, "msg": "elemento actualizado correctamente", "data": data});    
-            }else{
-                return res.status(404).json({"state": false, "msg": "elemento no encontrado"});
-            }
-        } catch (error) {
-            return res.status(500).json({"state": false, "error": "no available :v"});
-        }
     },
 
     deletear : async(req,res) => {
-        const {id} = req.params;
-        let founded;
         try{
-            for (let pos = 0; pos < datos.imagens.length; pos++) {
-                if(datos.imagens[pos].id == id){
-                    datos.imagens.splice(pos, 1);
-                    founded = true;
-                }
-            }
-
-            if(founded){
-                let datosJSON = JSON.stringify(datos);
-                fs.writeFileSync('data.json', datosJSON);
-                return res.status(200).json({"state": 200, "msg": "elemento eliminado correctamente"});    
-            }else{
-                return res.status(404).json({"state": false, "msg": "elemento no encontrado"});
-            }
+            const {id} = req.params;
+            await imagenModel.deleteOne( {id: id} );
+            return res.json({ message: 'Imagen eliminado' });
         }catch(error){
-            return res.status(500).json({"state": false, "error": "no available :v"});
+            return res.status(500).json({"state": false, "error": "no available :v", message: "imagen NO eliminada"});
         }
     }
 }
